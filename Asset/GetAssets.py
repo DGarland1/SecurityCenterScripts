8398fe93b85e74909c31dff07839ff206f33fa96
David Garland <david5@3discovery.com>
2018-04-13 09:42:56 -0500

    Initial commit of GetAssets script

---
+++ Asset/GetAssets.py
@@ -0,0 +1,275 @@
+"""-------------------------------------------------------------------------------
+ Purpose:     Dumps the Assets from SecurityCenter
+
+ Author:      DGarland
+-------------------------------------------------------------------------------
+
+Requirements:
+    dicttoxml and pysecuritycenter Python modules needs to be downloaded and installed
+
+    'pyLogging.py' file is a set of reusable code so that the scripts
+    can write to both console (when this script is ran from console) as well
+    as to a defined log file.
+
+    'pyCommon.py' file is a set of reusable code for various purposes.  Most of my
+    scripts make use of the functions in this Python script.
+"""
+
+# Import python modules
+import sys
+import os
+import getpass
+import getopt
+from configparser import ConfigParser
+
+# adds higher directory to python module path to import
+# custom modules one directory up
+sys.path.append(".")
+
+# Import reusable code dealing with setting up logging
+from pyLogging import clsLogging
+from pyCommon import writexml, writecsv
+
+#--- Prevent the creation of compiled import modules ---
+sys.dont_write_bytecode = True
+
+# --- Get location and name of script and store as variables ---
+scriptloc = os.path.join(os.path.dirname(os.path.realpath(__file__)), '')
+# What to save all files as (leave out file extension)
+scriptname = os.path.splitext(os.path.basename(__file__))[0]
+
+repoID = '0'  # Set repository ID to All
+filename = ''  # Initialize filename variable to empty
+optcsv = False
+
+# Get options passed via commandline
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'hcf:', [
+                               'help', 'csv', 'filename='])
+except getopt.GetoptError as err:
+    print('Example: Asset/GetAssets.py --csv')
+    print('Example: Asset/GetAssets.py -f "Assets"')
+    sys.exit(1)
+for opt, arg in opts:
+    if opt in ('-f', '--filename'):
+        filename = str(arg)
+    if opt in ('-h', '--help'):
+        print('Example: Asset/GetAssets.py --csv')
+        print('Example: Asset/GetAssets.py -f "Assets"')
+        sys.exit(0)
+    if opt in ('-c', '--csv'):
+        optcsv = True
+
+if filename:
+    scriptname = filename
+
+#--- Begin Logging Configuration Section ---
+# Initialize logging
+loginstance = clsLogging(scriptloc, scriptname)
+logger = loginstance.setup()
+
+logger.info('Running on Python version {}'.format(sys.version))
+
+
+def main():
+    configfile = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), '..', 'config.conf')
+    config = ConfigParser()
+
+    if not os.path.exists(configfile):
+        # Well there wasn't a config file located in the parent directory
+        # so we should create a new one.
+        config.add_section('SecurityCenter')
+        config.set('SecurityCenter', 'host', input(
+            'SecurityCenter IP Address : '))
+        config.set('SecurityCenter', 'user', input(
+            'SecurityCenter Username : '))
+        config.set('SecurityCenter', 'pass', getpass.getpass(
+            'SecurityCenter Password : '))
+        config.set('SecurityCenter', 'path', os.path.join(input(
+            'Folder to place reports : '), ''))
+
+        with open(configfile, 'w') as fobj:
+            config.write(fobj)
+    else:
+        config.read(configfile)
+
+    hostip = config.get('SecurityCenter', 'host')
+    username = config.get('SecurityCenter', 'user')
+    password = config.get('SecurityCenter', 'pass')
+    fldrloc = config.get('SecurityCenter', 'path')
+
+    data = getAssetData(hostip, username, password)
+
+    # What the element header for each set of data should be called
+    elementname = 'Assets'
+
+    # Write parse data (stored as dictionary objects in a list variable) to XML
+    try:
+        if optcsv:
+            writecsv(fldrloc, scriptname, data, logger)
+        else:
+            writexml(fldrloc, scriptname, data, elementname, logger)
+    except Exception:
+        logger.error('Error in writing the file', exc_info=True)
+        closeexit(1)
+
+    # Close log file and exit script cleanly
+    closeexit(0)
+
+
+def getAssetData(hostip, username, password):
+    '''Collect and parse the Assets from SecurityCenter and returns it at as a list of dictionaries
+
+    Parameters
+    ----------
+    hostip : str
+        IP address of SecurityCenter
+    username : str
+        Username with at least full read privileges in SecurityCenter
+    password : str
+        Password of user with at least full read privileges in SecurityCenter
+
+    Returns
+    -------
+    list: A list containing a series of asset dictionaries
+
+    References and Footnotes
+    ------------------------
+
+    Refer to the following sites for more information on how to build the query
+    pySecurityCenter Python Module
+
+    [1]https://github.com/SteveMcGrath/pySecurityCenter
+
+    [2]https://github.com/SteveMcGrath/pySecurityCenter/tree/master/examples/sc5
+
+    SecurityCenter 5 API
+
+    [3]https://support.tenable.com/support-center/cerberus-support-center/includes/widgets/sc_api/index.html
+
+    Tenable Community Forum for pySecurityCenter module
+
+    [4]https://community.tenable.com/search.jspa?q=pysecuritycenter
+
+    '''
+
+    try:
+        # Import SecurityCenter5 class from securitycenter module
+        from securitycenter import SecurityCenter5
+    except Exception:
+        # Log error and exit script
+        logger.error('Failed to import SecurityCenter module')
+        logger.error('Likely cause is that the securitycenter module has not been downloaded and installed. See https://pypi.python.org/pypi/pySecurityCenter', exc_info=True)
+        closeexit(1)
+
+    #--- Connect to SecurityCenter ---
+    try:
+        # Create connection to SecurityCenter server using variables stored in pyTenableConfig.py
+        # Results are returned as a series of dictionary objects within the 'sc' list variable
+        sc = SecurityCenter5(hostip)
+        sc.login(username, password)
+    except Exception:
+        # Log error and exit script
+        logger.error('Failed to connect to SecurityCenter server')
+        logger.error(
+            'Likely cause is that the credentials to login into SecurityCenter are incorrect', exc_info=True)
+        closeexit(1)
+
+    #--- Retrieve data from SecurityCenter and export to XML ---
+
+    try:
+        # Get data from SecurityCenter
+        logger.info('Getting list of data from SecurityCenter')
+        #
+        # Getting all the Accepted Risk Rules from SecurityCenter
+        # so there is no need to modify the 'resp' and 'rules' lines below
+        #
+        resp = sc.get('asset', params={
+            'fields': 'id,name,description,viewableIPs'
+        })
+
+        data = (resp.json()['response']['usable'])
+        # Each entry of data is returned as a dictionary variable stored in list 'rules'
+    except Exception:
+        # Problem with trying to get data from SecurityCenter
+        # Log error and exit script
+        logger.error('Failed to get data from SecurityCenter')
+        logger.error('Likely cause is the query is malformed', exc_info=True)
+        closeexit(1)
+
+    try:
+        if data is not None:  # If rules variable doesn't come back null/empty
+            return parsedata(data)
+        else:
+            logger.info('No assets found')
+            closeexit(0)
+    except Exception as e:
+        logger.error('Error parsing list of data in getAssetData function')
+        logger.error('Data string follows')
+        logger.error(e, exc_info=True)
+        closeexit(1)
+
+
+def parsedata(data):
+    """Parse out the collected list of data
+    Parameters
+    ----------
+    sc : obj
+        SecurityCenter connection
+
+    data : list
+        Collection of dictionaries from the SC analysis
+
+    Returns
+    -------
+    list: A list containing a series of parsed asset dictionaries
+    """
+
+    # Create a new list for the all data
+    newlist = []
+    # Create a new dictionary for the asset data
+    newdict = {}
+
+    # loop through each asset found
+    for asset in data:
+        ipLists = ''
+        if asset['id'] != '0':
+
+            for vIP in asset['viewableIPs']:
+                ipLists = ipLists + vIP['ipList']
+
+            ipLists = ipLists.rstrip('\n')
+            ipList = ipLists.split('\n')
+
+            if not '' in ipList:
+                for ip in ipList:
+                    iphost = ip.split('|')
+                    newdict['ip'] = iphost[0]
+                    newdict['assetName'] = asset['name']
+                    newdict['assetDesc'] = asset['description']
+                    newlist.append(newdict)  # append dictionary to list
+                    newdict = {}  # clear dictionary for next run through
+
+    return newlist
+
+
+def closeexit(exit_code):
+    """Function to handle exiting the script either cleanly or with an error
+
+    exit_code - 0 for clean exit, 1 for exiting due to script error
+    """
+
+    if exit_code == 0:  # Script completed without an error
+        logger.info('Script complete')
+    else:  # Script had an error
+        logger.info('Exiting script due to an error')
+
+    # Cleanly close the logging files
+    # Function below comes from pyLogging.py script
+    loginstance.closeHandlers()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Asset/README.md
@@ -0,0 +1,84 @@
+# Get Assets script
+*Important: See Requirements and Setup Instructions below before trying to run this script*
+
+This script exports the a list of asset groups and the IPs in those groups out of SecurityCenter 5 into a CSV or XML format (which you can easily open in Excel if need be).
+
+It is a basic script but helpful in identifying what IPs appear in an asset group.  This comes in really handy if you wish to identify what asset groups contain a particular IP or subnet of IPs.
+
+The XML and CSV export will provide the following fields:
+- **ip** - IP of the device in the asset.
+- **assetName** - Name of the asset the IP is assigned to.
+- **assetDesc** - Description of the asset.
+
+Log files for the script are stored in the same directory as the script itself.
+
+Script results are stored in whatever directory you signify.  See Setup Instructions below.
+
+## Requirements
+- Tenable SecurityCenter 5
+- Python 3 (script was designed using Python 3.6)
+
+Aside from the standard library of modules that come with Python 3 you will need to install the following modules:
+- [pySecurityCenter](https://github.com/SteveMcGrath/pySecurityCenter)
+- [dicttoxml](https://github.com/quandyfactory/dicttoxml)
+
+You'll also need the pyLogging.py and pyCommon.py files in the parent directory as well.
+
+To run this script, your folder structure should look like this
+
+    \---SecurityCenterScripts
+        |   pyCommon.py
+        |   pyLogging.py
+        |
+        \---Asset
+                GetAssets.py
+
+Hopefully I'm not forgetting anything.  Let me know if I am.
+
+If your system running Python has access to the internet, you can install the modules using the commands:
+```
+pip install pysecuritycenter
+pip install dicttoxml
+```
+
+If you need to install manually, I would recommend you go to the pySecurityCenter and dicttoxml GitHub sites directly and follow their instructions for a manual installation of those modules.  Both modules above have additional module dependencies that you'll need to install manually as well.
+
+## Files needed
+You'll need to download all these files:
+- GetAssets.py
+- pyCommon.py
+- pyLogging.py
+
+## Setup Instructions
+A config.conf file containing the IP address of your SecurityCenter server, a user account with at least full read privileges (Auditor), a password, and a folder location to export the file to.  This config.conf file will be required for all of my SecurityCenter scripts.
+
+If you don't already have the config.conf file, running the script from a command line for the first time you'll be asked a series of questions (IP, username, password, path) and the config.conf file will be built for you automatically and stored in the parent directory.
+
+See below for an example of the config.conf file:
+
+    [SecurityCenter]
+    host = 10.11.12.13
+    user = username
+    pass = password
+    path = C:\scripts\
+
+## Run Instructions
+Just run 'GetAssets.py' from your favorite Python IDE.
+
+Or you can run it from command line.  If you use the command line, you must run python from the parent directory.
+
+    python Asset/GetAssets.py
+
+There are also some optional arguments you can use as well:
+
+    --help | -h
+        Display a short help of example commands
+
+    --csv | -c
+        OPTIONAL. By default, the script exports the results as an XML file.  Setting this option tells the script to export the results as a CSV file instead.
+
+    --filename | -f <filename>
+        OPTIONAL. Name of the file to save the results to.  Do not include the extension of the filename as the file will
+        always be an XML file.
+
+If you have a fairly large SecurityCenter deployment, this script can take several minutes to complete.  So be patient.
